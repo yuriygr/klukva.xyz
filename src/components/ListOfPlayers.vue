c5fc18c7f942a622fd677f88d5a731127a4fd0b8
Yuriy Grinev <a1d516ac5f5d290@gmail.com>
2024-07-27 16:19:43 +0300

    Да

--- src/components/ListOfPlayers.vue
+++ src/components/ListOfPlayers.vue
@@ -1,6 +1,11 @@
 
 <template>
-	{{ players }}
+	<div class="players-online">{{ players.online }} / {{ players.max }}</div>
+	<div class="players-list">
+		<div class="player-item" v-for="player in players.list" :key="`player-${player.uuid}`">
+			<span>{{ player.name }}</span>
+		</div>
+	</div>
 </template>
 
 <script setup>
@@ -19,3 +24,24 @@ axios.get(`https://api.mcsrvstat.us/3/${props.address}`)
 })
 .catch(console.log)
 </script>
+
+<style lang="scss">
+.players-list {
+	display: grid;
+	position: relative;
+}
+
+.player-item {
+	span {
+		font-family: 'Minecraft';
+			color: #fff;
+			font-size: 1.2rem;
+
+			white-space: pre-line;
+			word-wrap: break-word;
+			word-break: break-word;
+			font-weight: 400;
+			line-height: calc(1.6 * 1em);
+	}
+}
+</style>
