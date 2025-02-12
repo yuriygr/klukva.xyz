217d48d5d3547767060fa929780d708079a1e6b9
Yuriy Grinev <a1d516ac5f5d290@gmail.com>
2025-02-12 20:45:59 +0300

    Двигаемся в сторону к распределенным серверам?

---
+++ src/components/server/index.js
@@ -0,0 +1,4 @@
+export { default as ServerAddress } from '@components/server/Address.astro';
+export { default as ServerPlayers } from '@components/server/Players.vue';
+export { default as ServerStatistics } from '@components/server/Statistics.vue';
+export { default as ServerStatus } from '@components/server/Status.vue';
--- src/content/worlds/old-world.md
+++ src/content/worlds/old-world.md
@@ -1,7 +1,7 @@
 ---
 title: "Старый мир"
 info: "Наш основной мир"
-address: "mc.klukva.xyz"
+address: "ow.klukva.xyz"
 icon: "old-world.jpg"
 backgroud_code: "dirt"
 tint: "#2e3d22"
